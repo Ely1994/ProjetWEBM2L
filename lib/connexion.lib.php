d1fdfa7f78c988bc0425facad956922b19307d8f
Ely1994 <31419057+Ely1994@users.noreply.github.com>
2018-03-05 17:16:33 +0100

    komit lambda

--- lib/connexion.lib.php
+++ lib/connexion.lib.php
@@ -12,11 +12,41 @@ function connexion() { // DATAACCES : connexion à la base de données dbpjr
 
 function M2LgetFormation() { // DATAACCES : accession à tout le contenu de formation
     $dbh = connexion();
-    $sql = "SELECT F_id, F_nom, F_description, F_lieu, F_prerequis FROM formation ORDER BY id DESC;";
+    $sql = 'SELECT F_id, F_nom, F_description, F_lieu, F_prerequis FROM formation WHERE date_Formation>CURDATE() ORDER BY id;';
+    /*  DESC */
     //$sql = "SELECT F_id, F_nom, F_description, F_lieu, F_prerequis, date_debut, duree_jour, duree_heure, DATE_ADD(date_debut, INTERVAL (duree_jour - 1) DAY) as date_fin FROM formation ORDER BY id DESC;";
+    $prep=$dbh->prepare($sql);
+    $resultat=$prep->execute(array());
+    $resultat=$prep->fetchAll();
+    print_r($resultat);
+    return $resultat;
+/* vieux code
     $values = $dbh->query($sql);
     $value2 = $values->fetchAll();
-	return $value2;
+    return $value2;
+*/
+/* le code de frank
+    $dbh=connexion();
+    $req='SELECT id_Formation,titre_Formation,description_forma,date_Formation,duree_Formation,nom_Prestataire,credit FROM Prestataire inner join Formation on Prestataire.id_Prestataire=Formation.id_Prestataire where date_Formation>CURDATE()';
+    $prep=$dbh->prepare($req);
+    $resultat=$prep->execute(array());
+    $resultat=$prep->fetchAll();
+    return $resultat;
+*/
+/* un code 
+    $dbh = connexion();
+    $sql = "SELECT F_id, F_nom, F_description, F_lieu, F_prerequis FROM formation WHERE :F_id = F_id AND :F_nom = F_nom AND :F_description = F_description AND :F_lieu = F_lieu AND :F_prerequis = F_prerequis;";
+    $stmt = $dbh->prepare($sql);
+    $stmt->BindValue(':F_id', $id);
+    $stmt->BindValue(':F_nom', $nom);
+    $stmt->BindValue(':F_description', $description);
+    $stmt->BindValue(':F_lieu', $lieu);
+    $stmt->BindValue(':F_prerequis', $prerequis);
+
+    $retour = $stmt->execute();
+    $retour = $stmt->fetchAll();
+    return $retour;
+    */
 }
 
 function connectmd5() { // code en md5
--- views/profil.php
+++ views/profil.php
@@ -39,22 +39,33 @@ if(isset($_COOKIE['holycookie'])==TRUE) {
     <link rel="stylesheet" href="../style/main.style.css" />
     <link rel="stylesheet" href="../style/header.style.css" />
     <link rel="stylesheet" href="../style/nav.style.css" />
+    <link rel="stylesheet" href="../style/section.style.css" />
     <link rel="stylesheet" href="../style/footer.style.css" />
     <script type="text/javascript" src="../javascript/lib.js"></script>    
-    <title>M2L - Profil</title>
+    <title>M2L - Page de profil</title>
 </head>
 <body>
     <?php include '../inc/header.inc.php'; ?>
     <?php include '../inc/nav.inc.php'; ?>
-
-        <section>
-            <h2>Bienvenue sur ta page de profil <?php echo $_SESSION['login']; ?>.</h2>
-        </section>
+    <section>
+        <div class="SE1_titre">Bienvenue sur ta page de profil <?php echo $_SESSION['login']; ?>.</div>
+</section>
         <section>
             <p>Tu en as marre de tes cookies ? clique 
             <input type="button" value="ici" onclick="alerte()">
              pour donner tout tes cookies à Darth Jar Jar Binks.</p>
         </section>
+    <section>
+        <div>
+            Salut. Je suis ton compteur de cookies. Tu dispose de <?php echo $_COOKIE['holycookie']; ?> cookies du bon côté de la force, ainsi que de <?php echo $_COOKIE['darkcookie']; ?> cookies du côté obscur de la force.
+            <?php if($_COOKIE['holycookie'] > $_COOKIE['darkcookie']) {
+                ?> Tu est donc un défenseur des jedi.<?php
+            } else {
+                ?> Tu est un sith qui cherche à détruire.. un truc.<?php
+            }
+            ?>
+        </div>
+    </section>
     <?php include '../inc/footer.inc.php'; ?>
 </body>
 </html>
